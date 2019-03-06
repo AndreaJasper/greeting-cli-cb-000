cf68881341fa29f2289abc9c6684850b343a29fd
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-06 21:54:41 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,9 @@
 # code the #greeting method here!
+<<<<<<< HEAD
 def greeting(name)
   puts "Hello #{name.strip}. It's nice to meet you."
+=======
+def greet(name)
+  puts
+>>>>>>> a8c004d46caa873592100198c81e8d94d121a47e
 end
